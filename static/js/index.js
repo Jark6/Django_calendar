ab756fd790dfc14050cb8e96e443c58f7417b617
Jark6 <enter_y@e1.ru>
2023-06-01 17:22:06 +0500

    3. default selected added

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,11 @@
 //let dp3 = new AirDatepicker("#datepicker-container-3");
 
 const container = document.getElementById('container');
+const dates = [
+  new Date('2023-06-01'),
+  new Date('2023-06-05'),
+  new Date('2023-06-10')
+];
 let rowSerial = 0;
 for (let i = 0; i < 12; i++) {
     if (i==0 || i==6){
@@ -20,16 +25,37 @@ for (let i = 0; i < 12; i++) {
   newContainer.classList.add('col');
   row.appendChild(newContainer);
   const dm=new Date(new Date().getFullYear(), i+1, 0).getDate();
-  new AirDatepicker(newContainer, {
+  const name = 'airDatepicker'+i;
+  let $name = new AirDatepicker(newContainer, {
     inline:true, 
     selectOtherMonth:false, 
     multipleDates:true, 
-    weekends:[6,0], 
-    showOtherMonths:false,  
+    weekends:[6,0],
+    showOtherMonths:false,
+    highlightWeekends:true,
     moveToOtherMonthsOnSelect: false,     
     navTitles: { days: '<strong >MMMM</strong>' },
     minDate:new Date(new Date().getFullYear(), i, 1),
     maxDate:new Date(new Date().getFullYear(), i, dm),
-    //selectedDates: [6,0]
+    onRenderCell: function (date, cellType) {
+        if (cellType === 'day') {
+            const day = date.getDay();
+            if (day === 6 || day === 0){
+            return{
+              classes: 'selected',
+            };
+            }
+        };
+        }
+    //selectedDates: dates,
   });
+  //$name.hide();
+    let docNode = document.querySelectorAll('div.-weekend-');
+    docNode.forEach(doc => doc.classList.add("-selected-"))
+
+    $name.selectDate(dates);
 }
+for (let i = 0; i < 12; i++) {
+    const name = 'airDatepicker'+i;
+    let $name;
+    }
